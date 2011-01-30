ab9d0347280f1b10d95c59ca2e45029f2e216285
msyk <nii@msyk.net>
2011-01-30 19:26:11 +0900

    0.6.1 missing

--- develop-im/sample_form_MySQL_include.php
+++ develop-im/sample_form_MySQL_include.php
@@ -11,31 +11,36 @@ require_once ( 'INTER-Mediator/INTER-Mediator.php');
 
 $tableDefs 
 	= array(	
+		array(	
+			'name' 	=> 'postalcode', 
+			'records'	=>	1000000,
+			'key' 		=> 'id',
+			'query'	=> array( array( 'field'=>'f9', 'operator'=>'Like', 'value'=>'%落合%' ) ),
+			'sort'	=> array( array( 'field'=>'f3', 'direction'=>'asc' ),),
+		),
 		array(	'records'	=>	2,
 				'name' 		=> 'person', 
 				'key' 		=> 'id',
 				'query'		=> array( /* array( 'field'=>'id', 'value'=>'5', 'operator'=>'eq' ),*/ ),
-				'sort'		=> array( array( 'field'=>'id', 'direction'=>'asc' ),),),
+				'sort'		=> array( array( 'field'=>'id', 'direction'=>'asc' ),),
+		),
 		array(	'name'			=> 'contact',
 				'key'			=> 'id',
 				'foreign-key'	=> 'person_id',
-				'join-field' 	=> 'id'),
+				'join-field' 	=> 'id',
+				'repeat-control'	=> 'insert delete',
+		),
 		array(	'name' 	=> 'contact_way',
 				 'key' 	=> 'id',),
-		array(	'name' 		=> 'cor_way_kindname', 
-				'key' 		=> 'id',
-				'foreign-key' => 'way_id',
+		array(	'name' 			=> 'cor_way_kindname', 
+				'key' 			=> 'id',
+				'foreign-key' 	=> 'way_id',
 				'join-field' 	=> 'way'),
 		array(	'name' 			=> 'history', 
 				'key' 			=> 'id',
 				'foreign-key'	=> 'person_id',
 				'repeat-control'	=> 'insert',
 				'join-field' 	=> 'id',	),
-		array(	
-			'name' 	=> 'postalcode', 
-			'query'	=> array( array( 'field'=>'f9', 'value'=>'落合', 'operator'=>'cn' ) ),
-			'sort'	=> array( array( 'field'=>'f3', 'direction'=>'ascend' ),),
-		),
 	);
 	
 $optionDefs
@@ -44,7 +49,10 @@ $optionDefs
 		)
 	);
 
-$dbDefs = array( 'db-class' => 'MySQL', 'db' => 'test_db' );
+$dbDefs = array( 'db-class' => 'PDO', 
+				 'dsn'=>'mysql:host=localhost;dbname=test_db',
+			//	'options'=>array(1002 /*PDO::MYSQL_ATTR_INIT_COMMAND*/ =>'set names utf8') 
+			);
 
 IM_Entry( $tableDefs, $optionDefs, $dbDefs, true );
 
