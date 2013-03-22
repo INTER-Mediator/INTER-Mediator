e1e027fce0309259c02443c213f405a53b080a29
msyk <nii@msyk.net>
2013-03-23 01:41:04 +0900

    Upload component is building

---
+++ develop-im/Demo-Script/demo3.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: msyk
+ * Date: 2013/03/20
+ * Time: 19:09
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ develop-im/Demo-Script/demo3_starter.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: msyk
+ * Date: 2013/03/22
+ * Time: 23:54
+ * To change this template use File | Settings | File Templates.
+ */
