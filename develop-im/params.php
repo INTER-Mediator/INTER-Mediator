14da82c486621835cc45c2556e326f681554b5f5
msyk <nii@msyk.net>
2013-04-03 02:42:22 +0900

    at the fni dir

---
+++ develop-im/params.php
@@ -0,0 +1,71 @@
+<?php
+/*
+* INTER-Mediator Ver.@@@@2@@@@ Released @@@@1@@@@
+*
+*   by Masayuki Nii  msyk@msyk.net Copyright (c) 2010 Masayuki Nii, All rights reserved.
+*
+*   This project started at the end of 2009.
+*   INTER-Mediator is supplied under MIT License.
+*/
+
+/* DB_FileMaker_FX awares below:
+ */
+$dbServer = '127.0.0.1';
+$dbPort = '80';
+$dbUser = 'webuser';
+$dbPassword = 'qrj6PK37';
+$dbDataType = 'FMPro7';
+$dbDatabase = 'HagakiPrint';
+$dbProtocol = 'HTTP';
+
+/* DB_PDO awares below:
+ */
+//$dbDSN = 'mysql:unix_socket=/tmp/mysql.sock;dbname=test_db;';
+//$dbOption = array();
+//$dbUser = 'web';
+//$dbPassword = 'password';
+
+/* Browser Compatibility Check:
+ */
+$browserCompatibility = array(
+    'msie' => '7+',
+    'FireFox' => '1+',
+    'Chrome' => '1+',
+    'Safari' => '1+',
+//    'Safari'=>array('Mac'=>'4+','Win'=>'4+'), // Sample for dividing with OS
+    'Opera' => '1+'
+);
+
+/* This statement set debug to false forcely. */
+$prohibitDebugMode = false;
+$prohibitSetCondition = true;
+$prohibitSetOrder = true;
+$prohibitSetWriteCriticalFields = true;
+//$prohibitIgnoreCondition = true;
+
+/*
+Command to generate the following RSA key:
+$ openssl genrsa -out gen.key 512
+
+*/
+$passPhrase = '';
+$generatedPrivateKey = <<<EOL
+-----BEGIN RSA PRIVATE KEY-----
+MIIBOwIBAAJBAKihibtt92M6A/z49CqNcWugBd3sPrW3HF8TtKANZd1EWQ/agZ65
+H2/NdL8H6zCgmKpYFTqFGwlYrnWrsbD1UxcCAwEAAQJAWX5pl1Q0D7Axf6csBg1M
+3V5u3qlLWqsUXo0ZtjuGDRgk5FsJOA9bkxfpJspbr2CFkodpBuBCBYpOTQhLUc2H
+MQIhAN1stwI2BIiSBNbDx2YiW5IVTEh/gTEXxOCazRDNWPQJAiEAwvZvqIQLexer
+TnKj7q+Zcv4G2XgbkhtaLH/ELiA/Fh8CIQDGIC3M86qwzP85cCrub5XCK/567GQc
+GmmWk80j2KpciQIhAI/ybFa7x85Gl5EAS9F7jYy9ykjeyVyDHX0liK+V1355AiAG
+jU6zr1wG9awuXj8j5x37eFXnfD/p92GpteyHuIDpog==
+-----END RSA PRIVATE KEY-----
+EOL;
+
+//$httpAccounts = array('user'=>'testtest');
+//$httpRedirectURL = "http://10.0.1.226/im/Sample_products/products_MySQL.html";
+
+// in case of $_SERVER['SCRIPT_NAME'] didn't return the valid path.
+// These are added before/after the path.
+//$scriptPathPrefix = "";
+//$scriptPathSufix = "";
+?>
