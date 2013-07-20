5e01d5fc739c04b012bbb4fb0ffc8ca8341e1a87
Atsushi Matsuo <famlog@gmail.com>
2013-07-21 01:03:18 +0900

    Add DB_Proxy_Test.php

---
+++ develop-im/INTER-Mediator-UnitTest/DB_Proxy_Test.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once('../INTER-Mediator/INTER-Mediator.php');
+require_once('../INTER-Mediator/DB_Interfaces.php');
+require_once('../INTER-Mediator/DB_Logger.php');
+require_once('../INTER-Mediator/DB_Settings.php');
+require_once('../INTER-Mediator/DB_UseSharedObjects.php');
+require_once('../INTER-Mediator/DB_Proxy.php');
+require_once('../INTER-Mediator/DB_Formatters.php');
+require_once('../INTER-Mediator/DB_AuthCommon.php');
+
+class DB_Proxy_Test extends PHPUnit_Framework_TestCase
+{
+    function setUp()
+    {
+        mb_internal_encoding('UTF-8');
+        date_default_timezone_set('Asia/Tokyo');
+
+        $this->db_proxy = new DB_Proxy(true);
+        $this->db_proxy->initialize(array(),
+            array(
+                'authentication' => array( // table only, for all operations
+                    'user' => array('user1'), // Itemize permitted users
+                    'group' => array('group2'), // Itemize permitted groups
+                    'privilege' => array(), // Itemize permitted privileges
+                    'user-table' => 'authuser', // Default value
+                    'group-table' => 'authgroup',
+                    'corresponding-table' => 'authcor',
+                    'challenge-table' => 'issuedhash',
+                    'authexpired' => '300', // Set as seconds.
+                    'storing' => 'cookie-domainwide', // 'cookie'(default), 'cookie-domainwide', 'none'
+                ),
+            ),
+            array(
+                'db-class' => 'PDO',
+                'dsn' => 'mysql:dbname=test_db;host=127.0.0.1',
+                'user' => 'web',
+                'password' => 'password',
+            ),
+            false);
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    function test___construct()    {
+        $testName = "Check __construct function in DB_Proxy.php.";
+        
+        ob_start();
+        $this->db_proxy->__construct();
+        $headers = xdebug_get_headers();
+        header_remove();
+        ob_clean();
+        
+        $this->assertContains('X-Content-Type-Options: nosniff', $headers);
+        $this->assertContains('X-Frame-Options: SAMEORIGIN', $headers);
+    }
+
+}
