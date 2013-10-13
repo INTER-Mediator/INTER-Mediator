eb58fc131cf311016302d199a2dae9bdc2c4cb06
msyk <nii@msyk.net>
2013-10-13 12:21:12 +0900

    Conflict resolve for include_FMS2.php

--- develop-im/Sample_form/include_FMS2.php
+++ develop-im/Sample_form/include_FMS2.php
@@ -15,7 +15,6 @@ IM_Entry(
             'records' => 1,
             'paging' => true,
             'name' => 'person_layout',
-          //  'key' => '-recid',
             'repeat-control' => 'confirm-delete confirm-insert',
             'query' => array( /* array( 'field'=>'id', 'value'=>'5', 'operator'=>'eq' ),*/),
             'sort' => array(
@@ -26,16 +25,10 @@ IM_Entry(
         array(
             'name' => 'contact_to',
             'view' => 'person_layout',
-          //  'key' => '-recid',
             'repeat-control' => 'confirm-delete insert',
             'relation' => array(
-                //array('foreign-key' => 'person_id', 'join-field' => 'id', 'operator' => 'eq')
                 array('portal' => true)
             ),
-            //'sort' => array(
-            //    array('field' => 'id', 'direction' => 'ascend'
-            //    ),
-            //),
         ),
         array(
             'name' => 'contact_way',
