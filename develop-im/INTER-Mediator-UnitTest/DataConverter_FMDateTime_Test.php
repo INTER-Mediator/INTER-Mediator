3d75ef6591f10e594c101ce3e0c064d36e789658
Atsushi Matsuo <famlog@gmail.com>
2013-10-21 11:34:44 +0900

    Update DataConverter_FMDateTime_Test.php and DataConverter_MySQLDateTime_Test.php

--- develop-im/INTER-Mediator-UnitTest/DataConverter_FMDateTime_Test.php
+++ develop-im/INTER-Mediator-UnitTest/DataConverter_FMDateTime_Test.php
@@ -9,27 +9,25 @@ class DataConverter_FMDateTime_Test extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        $_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'ja';
+        $_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'en';
         
         $this->dataconverter = new DataConverter_FMDateTime();
     }
     
     public function test_converterFromDBtoUser()
     {
-        $testName = "Check converterFromDBtoUser function in DataConverter_FMDateTime.php.";
+        $testName = 'Check converterFromDBtoUser function in DataConverter_FMDateTime.php.';
         
-        $datetimeString = "01/05/2000 12:34:56";
-        //$convertedDatetimeString = "2000/01/05 12:34:56";
-        $convertedDatetimeString = "01/05/00 12:34:56";  // for Travis CI
+        $datetimeString = '01/05/2000 12:34:56';
+        $convertedDatetimeString = strftime('%x %H:%M:%S', strtotime('01/05/00 12:34:56'));
         $this->assertSame($this->dataconverter->converterFromDBtoUser($datetimeString), $convertedDatetimeString, $testName);
 
-        $dateString = "01/05/2000";
-        //$convertedDateString = "2000/01/05";
-        $convertedDateString = "01/05/00";  // for Travis CI
+        $dateString = '01/05/2000';
+        $convertedDatetimeString = strftime('%x', strtotime('01/05/00'));
         $this->assertSame($this->dataconverter->converterFromDBtoUser($dateString), $convertedDateString, $testName);
 
-        $timeString = "12:34:56";
-        $convertedTimeString = "12:34:56";
+        $timeString = '12:34:56';
+        $convertedTimeString = '12:34:56';
         $this->assertSame($this->dataconverter->converterFromDBtoUser($timeString), $convertedTimeString, $testName);
     }
 }
--- develop-im/INTER-Mediator-UnitTest/DataConverter_MySQLDateTime_Test.php
+++ develop-im/INTER-Mediator-UnitTest/DataConverter_MySQLDateTime_Test.php
@@ -9,27 +9,25 @@ class DataConverter_MySQLDateTime_Test extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        $_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'ja';
+        $_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'en';
         
         $this->dataconverter = new DataConverter_MySQLDateTime();
     }
     
     public function test_converterFromDBtoUser()
     {
-        $testName = "Check converterFromDBtoUser function in DataConverter_MySQLDateTime.php.";
+        $testName = 'Check converterFromDBtoUser function in DataConverter_MySQLDateTime.php.';
         
-        $datetimeString = "2000-01-05 12:34:56";
-        //$convertedDatetimeString = "2000/01/05 12:34:56";
-        $convertedDatetimeString = "01/05/00 12:34:56";  // for Travis CI
+        $datetimeString = '01/05/2000 12:34:56';
+        $convertedDatetimeString = strftime('%x %H:%M:%S', strtotime('01/05/00 12:34:56'));
         $this->assertSame($this->dataconverter->converterFromDBtoUser($datetimeString), $convertedDatetimeString, $testName);
-
-        $dateString = "2000-01-05";
-        //$convertedDateString = "2000/01/05";
-        $convertedDateString = "01/05/00";  // for Travis CI
+        
+        $dateString = '01/05/2000';
+        $convertedDatetimeString = strftime('%x', strtotime('01/05/00'));
         $this->assertSame($this->dataconverter->converterFromDBtoUser($dateString), $convertedDateString, $testName);
 
-        $timeString = "12:34:56";
-        $convertedTimeString = "12:34:56";
+        $timeString = '12:34:56';
+        $convertedTimeString = '12:34:56';
         $this->assertSame($this->dataconverter->converterFromDBtoUser($timeString), $convertedTimeString, $testName);
     }
 }
