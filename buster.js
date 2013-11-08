781b0e45c0dcf0b58a5830e980d3dbd8420189f4
msyk <nii@msyk.net>
2013-11-09 01:13:35 +0900

    JS Unit test can be work on local machine (especially mac)

--- buster.js
+++ buster.js
@@ -3,7 +3,7 @@ var config = module.exports;
 config["My tests"] = {
     environment: "browser",
     sources: [
-        "develop-im/INTER-Mediator/*.js",
+        "develop-im/INTER-Mediator/*.js"
     ],
     tests: [
         "develop-im/INTER-Mediator-UnitTest/*-test.js"
