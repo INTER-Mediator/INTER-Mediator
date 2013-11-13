0dfba75b30a11a1930b65db182215d27c6075e81
Atsushi Matsuo <famlog@gmail.com>
2013-11-13 22:48:09 +0900

    Add MediaAccess_Test.php

---
+++ develop-im/INTER-Mediator-UnitTest/MediaAccess_Test.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * MediaAccess_Test file
+ */
+require_once(__DIR__ . '/../INTER-Mediator/MediaAccess.php');
+
+class MediaAccess_Test extends PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->mediaaccess = new MediaAccess();
+    }
+    
+    public function test_getMimeType()
+    {
+        $this->reflectionMethod = new ReflectionMethod('MediaAccess', 'getMimeType');
+        $this->reflectionMethod->setAccessible(true);
+
+        $path = '';
+        $expected = 'application/octet-stream';
+        $this->assertEquals($this->reflectionMethod->invokeArgs($this->mediaaccess, array($path)), $expected);
+
+        $path = 'test.jpg';
+        $expected = 'image/jpeg';
+        $this->assertEquals($this->reflectionMethod->invokeArgs($this->mediaaccess, array($path)), $expected);
+    }
+}
