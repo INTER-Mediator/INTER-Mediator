5fd2353bf08c8bd3583cd769543fc43fbe7b9f39
Atsushi Matsuo <famlog@gmail.com>
2013-11-22 01:37:50 +0900

    Add List-Detail Style Navigation Pages Sample for PostgreSQL

---
+++ develop-im/Sample_products/products_PostgreSQL_include.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * INTER-Mediator Ver.@@@@2@@@@ Released @@@@1@@@@
+ * 
+ *   by Masayuki Nii  msyk@msyk.net Copyright (c) 2013 Masayuki Nii, All rights reserved.
+ * 
+ *   This project started at the end of 2009.
+ *   INTER-Mediator is supplied under MIT License.
+ */
+require_once(dirname(__FILE__) . '/../INTER-Mediator/INTER-Mediator.php');
+
+IM_Entry(
+    array(
+        array(
+            'records' => 10,
+            'name' => 'product',
+            'key' => 'id',
+            'query' => array(array('field' => 'name', 'value' => '%', 'operator' => 'LIKE')),
+            'sort' => array(array('field' => 'name', 'direction' => 'ASC'),),
+        ),
+    ),
+    array(
+        'formatter' => array(
+            array('field' => 'product@unitprice',
+                'converter-class' => 'Number',
+                'parameter' => '0'),
+        ),
+    ),
+    array(
+        'db-class' => 'PDO',
+        'dsn' => 'pgsql:host=localhost;port=5432;dbname=test_db',
+    ),
+    false
+);
