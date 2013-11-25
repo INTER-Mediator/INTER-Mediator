7dddf8056480500f0c72fb685c9b3c768126d1d4
Atsushi Matsuo <famlog@gmail.com>
2013-11-26 01:49:45 +0900

    Replace repeated 'for' attribute of the 'label' elements to edit a correct checkbox of the record in repeaters (related commits: 6b59acd523c239faf54966d3755144f078dcf923, 014c319262ddd4d95ee187c02f84f8681753471d)

--- develop-im/INTER-Mediator/INTER-Mediator.js
+++ develop-im/INTER-Mediator/INTER-Mediator.js
@@ -1319,7 +1319,8 @@ var INTERMediator = {
                 nodeTag, typeAttr, linkInfoArray, RecordCounter, valueChangeFunction, nInfo, curVal,
                 curTarget, postCallFunc, newlyAddedNodes, keyingValue, oneRecord, isMatch, pagingValue,
                 recordsValue, currentWidgetNodes, widgetSupport, nodeId, nameAttr, nameNumber, nameTable,
-                selectedNode, foreignField, foreignValue, foreignFieldValue, dbspec, condition, optionalCondition = [];
+                selectedNode, replacedNode, children, foreignField, foreignValue, foreignFieldValue, dbspec,
+                condition, optionalCondition = [];
 
             currentLevel++;
             INTERMediator.currentEncNumber++;
@@ -1504,7 +1505,21 @@ var INTERMediator = {
 
                         for (k = 0; k < currentLinkedNodes.length; k++) {
                             // for each linked element
-                            setIdValue(currentLinkedNodes[k]);
+                            nodeId = currentLinkedNodes[k].getAttribute("id");
+                            replacedNode = setIdValue(currentLinkedNodes[k]);
+                            
+                            if (Object.keys(targetRecords.recordset).length > 1) {
+                                if (replacedNode.getAttribute("type") == "checkbox") {
+                                    children = replacedNode.parentNode.childNodes;
+                                    for (i = 0; i < children.length; i++) {
+                                        if (children[i].nodeType === 1 && children[i].tagName == "LABEL"
+                                                && nodeId == children[i].getAttribute("for")) {
+                                            children[i].setAttribute("for", replacedNode.getAttribute("id"));
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
                         }
                         for (k = 0; k < currentWidgetNodes.length; k++) {
                             var wInfo = INTERMediatorLib.getWidgetInfo(currentWidgetNodes[k]);
