0d5162f0a9123f09ea7a86107143935b85fa3f2c
msyk <nii@msyk.net>
2013-11-30 20:35:07 +0900

    Another trial.

--- buster.js
+++ buster.js
@@ -3,8 +3,8 @@ var config = module.exports;
 config["My tests"] = {
     environment: "browser",
     sources: [
-        "INTER-Mediator/*.js",
-        "INTER-Mediator/lib/js_lib/*.js"
+        "*.js",
+        "lib/js_lib/*.js"
     ],
     tests: [
         "INTER-Mediator-UnitTest/*-test.js"
