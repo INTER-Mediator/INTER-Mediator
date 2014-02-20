acef87283ef4d05a6f69567c86801a5c756656a0
msyk <nii@msyk.net>
2014-02-21 03:29:06 +0900

    Script to build the deployment set, updated.

--- dist-docs/buildup.sh
+++ dist-docs/buildup.sh
@@ -1,115 +1,186 @@
 #!/bin/sh
 
-# INTER-Mediator Distribution File Builder by Masayuki Nii
-#    Execute for current directory as the root of repository.
+# INTER-Mediator Deployment File Set Builder
+# by Masayuki Nii  msyk@msyk.net Copyright (c) 2010-2014 Masayuki Nii, All rights reserved.
 
-YUICOMP="../yuicompressor-2.4.7.jar"
+# This project started at the end of 2009.
+# INTER-Mediator is supplied under MIT License.
 
 version="4.1.6"
 
+# The jar file of YUI can be donwloaded from below.
+# http://grepcode.com/snapshot/repo1.maven.org/maven2/com.yahoo.platform.yui/yuicompressor/2.4.7
+#
+YUICOMP="yuicompressor-2.4.7.jar"
+YUICOMPLOG="yuicomp.log"
+buildRootName="im_build"
+imRootName="INTER-Mediator"
+receipt="receipt.txt"
+
+echo "================================================="
+echo " Start to build the INTER-Mediator Ver.${version}"
+echo "-------------------------------------------------"
+
 dt=`date "+%Y-%m-%d"`
 versionInFilename=`echo "${version}" | tr '.' '_'`;
+
 sedrule="/tmp/sedrule"
-
-curpath=$(cd $(dirname "$0"); pwd)
-curpath=$(dirname "${curpath}")
-echo "Working Directory is: ${curpath}"
-cd "${curpath}"
-
 cat << EOF > "${sedrule}"
 s/@@@@1@@@@/${dt}/
 s/@@@@2@@@@/${version}/
 EOF
 
-if [ -d "../temp" ]; then
-    rm -r ../temp
-fi
-mkdir ../temp
-cd ../temp
+distDocDir=$(cd $(dirname "$0"); pwd)
+originalPath=$(dirname "${distDocDir}")
 
-for aFile in $(ls "${curpath}"/*.php)
+topOfDir=$(dirname "${originalPath}")
+buildDir="${topOfDir}/${buildRootName}"
+buildPath="${buildDir}/${imRootName}"
+
+echo " Original: ${originalPath}"
+echo " Build to: ${buildPath}"
+
+echo "-------------------------------------------------"
+echo "Choose the build result from these:"
+echo " (1) Complete (everything contains)"
+echo " (2) Core only (the least set to work wep applications)"
+echo " (3) Core + Support (add Auth_Support and INTER-Mediator-Support)"
+/bin/echo -n "Type 1, 2 or 3, and then type return----> "
+read choice
+echo ""
+
+if [ -d "${buildDir}" ]; then
+    rm -r "${buildDir}"
+fi
+mkdir -p "${buildPath}"
+
+echo "PROCESSING: Copying php files in root"
+cd "${originalPath}"
+for aFile in $(ls *.php)
 do
-    filename=$(basename ${aFile})
-    echo "SED:${aFile}"
-    sed -f "${sedrule}" "${aFile}" > "./${filename}"
+    filename=$(basename "${aFile}")
+#    echo "SED: ${aFile}"
+    sed -f "${sedrule}" "${aFile}" > "${buildPath}/${filename}"
 done
-cp "${curpath}/index.html" .
-cp "${curpath}/readme.md" .
 
 #### Merge js files
-if [ -f ${YUICOMP} ]; then
-    sed -f "${sedrule}" "${curpath}/Adapter_DBServer.js" > temp.js
-else
-    cat ${curpath}/Adapter_DBServer.js      > temp.js
-fi
-cat ${curpath}/INTER-Mediator-Lib.js   >> temp.js
-cat ${curpath}/INTER-Mediator-Page.js  >> temp.js
-cat ${curpath}/INTER-Mediator-Parts.js >> temp.js
-sed -f "${sedrule}" ${curpath}/INTER-Mediator.js >> temp.js
-cat ${curpath}/lib/js_lib/sha1.js          >> temp.js
-cat ${curpath}/lib/js_lib/sha256.js        >> temp.js
-cat ${curpath}/lib/bi2php/biBigInt.js      >> temp.js
-cat ${curpath}/lib/bi2php/biMontgomery.js  >> temp.js
-cat ${curpath}/lib/bi2php/biRSA.js         >> temp.js
+echo "PROCESSING: Merging JS files"
+sed -f "${sedrule}" "${originalPath}/Adapter_DBServer.js" > "${buildPath}/temp.js"
+cat ${originalPath}/INTER-Mediator-Lib.js   >> "${buildPath}/temp.js"
+cat ${originalPath}/INTER-Mediator-Page.js  >> "${buildPath}/temp.js"
+cat ${originalPath}/INTER-Mediator-Parts.js >> "${buildPath}/temp.js"
+sed -f "${sedrule}" ${originalPath}/INTER-Mediator.js >> "${buildPath}/temp.js"
+cat ${originalPath}/lib/js_lib/sha1.js          >> "${buildPath}/temp.js"
+cat ${originalPath}/lib/js_lib/sha256.js        >> "${buildPath}/temp.js"
+cat ${originalPath}/lib/bi2php/biBigInt.js      >> "${buildPath}/temp.js"
+cat ${originalPath}/lib/bi2php/biMontgomery.js  >> "${buildPath}/temp.js"
+cat ${originalPath}/lib/bi2php/biRSA.js         >> "${buildPath}/temp.js"
+
+sed -f "${sedrule}" "${buildPath}/temp.js" > "${buildPath}/INTER-Mediator.js"
 
 #### Compress INTER-Mediator.js
-if [ -f ${YUICOMP} ]; then
-    sed '1s/*/*!/' temp.js > INTER-Mediator.js
-    java -jar ${YUICOMP} INTER-Mediator.js -v --charset UTF-8 -o temp.js
-    sed '1s/*!/*/' temp.js > "INTER-Mediator.js"
-else
-    sed -f "${sedrule}" "temp.js" > "INTER-Mediator.js"
+if [ -f "${topOfDir}/${YUICOMP}" ]; then
+    sed '1s/*/*!/' "${buildPath}/temp.js" > "${buildPath}/temp2.js"
+    java -jar "${topOfDir}/${YUICOMP}" "${buildPath}/temp2.js" -v --charset UTF-8 -o "${buildPath}/temp3.js" 2> "${buildDir}/${YUICOMPLOG}"
+    sed '1s/*!/*/' "${buildPath}/temp3.js" > "${buildPath}/INTER-Mediator.js"
 fi
-rm temp.js
+rm  "${buildPath}/temp.js" "${buildPath}/temp2.js" "${buildPath}/temp3.js"
 
-cp -rf "${curpath}/dist-docs" .
-mkdir lib
-mkdir lib/bi2php
-cp "${curpath}/lib/bi2php/biRSA.php" ./lib/bi2php
-cp -rf "${curpath}/lib/FX" ./lib
-cp -rf "${curpath}/lib/phpseclib" ./lib
+# Copy "lib" path php contents.
+echo "PROCESSING: ${originalPath}/lib"
+mkdir -p "${buildPath}/lib/bi2php"
+cp "${originalPath}/lib/bi2php/biRSA.php" "${buildPath}/lib/bi2php"
+cp -rf "${originalPath}/lib/FX" "${buildPath}/lib"
+cp -rf "${originalPath}/lib/phpseclib" "${buildPath}/lib"
+cp -rf "${originalPath}/lib/mailsend" "${buildPath}/lib"
 
-dirs="Auth_Support INTER-Mediator-Support INTER-Mediator-UnitTest Samples"
+if [ $choice = 3 ]; then
+    dirs=""
+elif [ $choice = 2 ]; then
+    dirs="Auth_Support INTER-Mediator-Support"
+else
+    dirs="Auth_Support INTER-Mediator-Support INTER-Mediator-UnitTest Samples"
+fi
 
 for TARGET in ${dirs}
 do
-mkdir ${TARGET}
-for DIR in $(ls "${curpath}/${TARGET}")
-do
-    if [ -f "${curpath}/${TARGET}/${DIR}" ]; then
-        echo "SED:${curpath}/${TARGET}/${DIR}"
-        sed -f "${sedrule}" "${curpath}/${TARGET}/${DIR}" > "${TARGET}/${DIR}"
-    else
-        mkdir "Samples/${DIR}"
-        for FILE in `ls "${curpath}/${TARGET}/${DIR}"`
-        do
-            if [ -f "${curpath}/${TARGET}/${DIR}/${FILE}" ]; then
-                case "${FILE}" in
-                    *\.html | *\.php | *\.js | *\.css | *\.txt)
-                        echo "SED:${curpath}/${TARGET}/${DIR}/${FILE}"
-                        sed -f "${sedrule}" "${curpath}/${TARGET}/${DIR}/${FILE}" > "${TARGET}/${DIR}/${FILE}"
-                        ;;
-                    *)
-                        echo "CP:${curpath}/${TARGET}/${DIR}/${FILE}"
-                        cp "${curpath}/${TARGET}/${DIR}/${FILE}" "${TARGET}/${DIR}/${FILE}"
-                        ;;
-                esac
-            fi
-        done
-    fi
+    echo "PROCESSING: ${originalPath}/${TARGET}"
+    mkdir -p "${buildPath}/${TARGET}"
+    for DIR in $(ls "${originalPath}/${TARGET}/")
+    do
+#        echo "Processing: ${originalPath}/${TARGET}/${DIR}"
+        if [ -f "${originalPath}/${TARGET}/${DIR}" ]; then
+#            echo "SED: ${originalPath}/${TARGET}/${DIR}"
+            sed -f "${sedrule}" "${originalPath}/${TARGET}/${DIR}" > "${buildPath}/${TARGET}/${DIR}"
+        else
+            mkdir -p "${buildPath}/${TARGET}/${DIR}"
+            for FILE in $(ls "${originalPath}/${TARGET}/${DIR}")
+            do
+                if [ -f "${originalPath}/${TARGET}/${DIR}/${FILE}" ]; then
+                    case "${FILE}" in
+                        *\.html | *\.php | *\.js | *\.css | *\.txt)
+#                          echo "SED: ${originalPath}/${TARGET}/${DIR}/${FILE}"
+                            sed -f "${sedrule}" "${originalPath}/${TARGET}/${DIR}/${FILE}" > "${buildPath}/${TARGET}/${DIR}/${FILE}"
+                            ;;
+                        *)
+#                            echo "CP: ${originalPath}/${TARGET}/${DIR}/${FILE}"
+                            cp "${originalPath}/${TARGET}/${DIR}/${FILE}" "${buildPath}/${TARGET}/${DIR}/${FILE}"
+                            ;;
+                    esac
+                fi
+            done
+        fi
+    done
 done
-done
 
-# Invalid the definition file of the DefEditor.
-defeditdeffile="INTER-Mediator-Support/defedit.php"
-sed 's|IM_Entry|/* IM_Entry|' "${defeditdeffile}" > /tmp/defedit.php
-cp /tmp/defedit.php "${defeditdeffile}"
+if [ $choice = 1 ]; then
+    echo "PROCESSING: ${originalPath}/readme.md"
+    cp     "${originalPath}/readme.md" "${buildPath}"
 
-cp -r "${curpath}"/Samples/Sample_products/images   Samples/Sample_products/
-cp -r "${curpath}"/Samples/WebSite/previous_rsrcs   Samples/WebSite/
+    echo "PROCESSING: ${originalPath}/dist-docs"
+    cp -rf "${originalPath}/dist-docs" "${buildPath}"
 
-find . -name "\.*" -exec rm -rf {} \;
+    echo "PROCESSING: Rest of ${originalPath}/Samples"
+    cp -r  "${originalPath}/Samples/Sample_products/images" "${buildPath}/Samples/Sample_products/"
+    cp -r  "${originalPath}/Samples/WebSite/previous_rsrcs" "${buildPath}/Samples/WebSite/"
 
-zip -r ../INTER-Mediator-${versionInFilename}.zip *
+# Invalidate the definition file of the DefEditor.
+    echo "PROCESSING: Invalidate the Definition File Editor for security reason."
+    defeditdeffile="${buildPath}/INTER-Mediator-Support/defedit.php"
+    sed 's|IM_Entry|/* IM_Entry|' "${defeditdeffile}" > /tmp/defedit.php
+    cp /tmp/defedit.php "${defeditdeffile}"
+fi
+
+find "${buildPath}" -name "\.*" -exec rm -rf {} \;
+
+# zip -r ../INTER-Mediator-${versionInFilename}.zip *
+
 rm "${sedrule}"
 
+#
+echo ""
+echo "=================================================" >> "${buildDir}/${receipt}"
+echo " INTER-Mediator Ver.${version} was successfully Build" >> "${buildDir}/${receipt}"
+echo " Check out: ${buildDir}" >> "${buildDir}/${receipt}"
+echo "=================================================" >> "${buildDir}/${receipt}"
+echo "Date: $(date)" >> "${buildDir}/${receipt}"
+echo "Original: ${originalPath}" >> "${buildDir}/${receipt}"
+echo "Build to: ${buildPath}" >> "${buildDir}/${receipt}"
+if [ $choice = 1 ]; then
+    echo "Your Choice: (1) Complete (everything contains)" >> "${buildDir}/${receipt}"
+elif [ $choice = 2 ]; then
+    echo "Your Choice: (2) Core only (the least set to work wep applications)" >> "${buildDir}/${receipt}"
+elif [ $choice = 3 ]; then
+    echo "Your Choice: (3) Core + Support (add Auth_Support and INTER-Mediator-Support)" >> "${buildDir}/${receipt}"
+else
+    echo "Your Choice: (1) Complete (everything contains)" >> "${buildDir}/${receipt}"
+fi
+echo "" >> "${buildDir}/${receipt}"
+echo "You can deploy the 'INTER-Mediator' folder into your web applications. Enjoy!!" >> "${buildDir}/${receipt}"
+echo "" >> "${buildDir}/${receipt}"
+echo "INTER-Mediator Web Site: http://inter-mediator.org" >> "${buildDir}/${receipt}"
+
+echo "================================================="
+echo " INTER-Mediator Ver.${version} was successfully Build"
+echo " Check out: ${buildDir}"
+echo "================================================="
