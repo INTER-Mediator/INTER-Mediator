f5c85efd8546b7408b4e9bc789a02e81994691aa
Atsushi Matsuo <famlog@gmail.com>
2014-02-25 04:26:43 +0900

    Change DataConverter_HTMLString.php to support disabling HTML escaping (by specifying 'parameter' => 'noescape')

--- INTER-Mediator-UnitTest/DataConverter_HTMLString_Test.php
+++ INTER-Mediator-UnitTest/DataConverter_HTMLString_Test.php
@@ -12,8 +12,12 @@ class DataConverter_HTMLString_Test extends PHPUnit_Framework_TestCase
         $_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'ja';
         
         $this->dataconverter = new DataConverter_HTMLString();
+        $this->dataconverter2 = new DataConverter_HTMLString(false);
+        $this->dataconverter3 = new DataConverter_HTMLString('false');
         $this->dataconverterForLinking = new DataConverter_HTMLString(true);
-        $this->dataconverterWithoutEscaping = new DataConverter_HTMLString(false, false);
+        $this->dataconverterForLinking2 = new DataConverter_HTMLString('true');
+        $this->dataconverterForLinking3 = new DataConverter_HTMLString('autolink');
+        $this->dataconverterWithoutEscaping = new DataConverter_HTMLString('noescape');
     }
     
     public function test_converterFromUserToDB()
@@ -68,11 +72,19 @@ class DataConverter_HTMLString_Test extends PHPUnit_Framework_TestCase
         $expected = '&amp;';
         $string = '&';
         $this->assertSame($expected, $this->dataconverter->converterFromDBtoUser($string));
-        
+
+        $expected = 'http://inter-mediator.org/';
+        $string = 'http://inter-mediator.org/';
+        $this->assertSame($expected, $this->dataconverter->converterFromDBtoUser($string));
+        $this->assertSame($expected, $this->dataconverter2->converterFromDBtoUser($string));
+        $this->assertSame($expected, $this->dataconverter3->converterFromDBtoUser($string));
+
         $expected = '<a href="http://inter-mediator.org/" target="_blank">http://inter-mediator.org/</a>';
         $string = 'http://inter-mediator.org/';
         $this->assertSame($expected, $this->dataconverterForLinking->converterFromDBtoUser($string));
-        
+        $this->assertSame($expected, $this->dataconverterForLinking2->converterFromDBtoUser($string));
+        $this->assertSame($expected, $this->dataconverterForLinking3->converterFromDBtoUser($string));
+
         $expected = '<a href="http://inter-mediator.org/" target="_blank">http://inter-mediator.org/</a><br />';
         $string = '<a href="http://inter-mediator.org/" target="_blank">http://inter-mediator.org/</a>' . "\n";
         $this->assertSame($expected, $this->dataconverterWithoutEscaping->converterFromDBtoUser($string));
