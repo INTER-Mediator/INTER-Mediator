8f55968fcf4948cf663d1098443a322907c0bd64
Atsushi Matsuo <famlog@gmail.com>
2014-03-09 16:38:37 +0900

    Add Asset Management Sample for SQLite

---
+++ Samples/Sample_Asset/contexts_SQLite.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ * INTER-Mediator Ver.@@@@2@@@@ Released @@@@1@@@@
+ *
+ *   by Masayuki Nii  msyk@msyk.net Copyright (c) 2010-2014 Masayuki Nii, All rights reserved.
+ *
+ *   This project started at the end of 2009.
+ *   INTER-Mediator is supplied under MIT License.
+ */
+require_once(dirname(__FILE__) . '/../../INTER-Mediator.php');
+
+IM_Entry(
+    array(
+        array(
+            'name' => 'asset',
+            'view' => 'asset',
+            'key' => 'asset_id',
+            'repeat-control'=>'insert delete',
+            'records' => 5,
+            'paging' => true,
+            'sort' => array(
+                array('field' => 'purchase', 'direction' => 'ASC'),
+            ),
+            'default-values'=>array(
+                array('field'=>'purchase', 'value'=> strftime('%Y-%m-%d')),
+            )
+        ),
+        array(
+            'name' => 'asseteffect',
+            'view' => 'asset',
+            'sort' => array(
+                array('field' => 'purchase', 'direction' => 'ASC'),
+            ),
+            'query' => array(
+                array('field' => 'discard', 'operator' => 'IS NULL'),
+            ),
+            'repeat-control'=>'insert delete',
+            'records' => 5,
+            'paging' => true,
+        ),
+        array(
+            'name' => 'assetdetail',
+            'view' => 'asset',
+            'table' => 'asset',
+            'records' => 1,
+            'key' => 'asset_id',
+        ),
+        array(
+            'name' => 'rent',
+            'key' => 'rent_id',
+            'sort' => array(
+                array('field' => 'rentdate', 'direction' => 'ASC'),
+            ),
+            'relation' => array(
+                array('foreign-key' => 'asset_id', 'join-field'=> 'asset_id', 'operator' => '='),
+            ),
+            'repeat-control'=>'insert delete',
+            'default-values'=>array(
+                array('field'=>'rentdate', 'value'=> strftime('%Y-%m-%d')),
+            )
+        ),
+        array(
+            'name' => 'staff',
+        ),
+        array(
+            'name' => 'rentback',
+            'table' => 'rent',
+            'key' => 'rent_id',
+            'query' => array(
+                array('field' => 'backdate', 'operator' => 'IS NULL'),
+            ),
+        ),
+        array(
+            'name' => 'category',
+        ),
+        array(
+            'name' => 'category-in-list',
+            'view' => 'category',
+            'relation' => array (
+                array('foreign-key' => 'category_id', 'join-field'=> 'category', 'operator' => '=')
+            )
+        ),
+    ),
+    array(
+        'formatter' => array(
+            array('field' => 'asset@purchase', 'converter-class' => 'MySQLDateTime', 'parameter'=>'%y/%m/%d'),
+            array('field' => 'asset@discard', 'converter-class' => 'MySQLDateTime'),
+        ),
+    ),
+    array(
+        'db-class' => 'PDO',
+        'dsn' => 'sqlite:/var/db/im/sample.sq3',
+    ),
+    false
+);
