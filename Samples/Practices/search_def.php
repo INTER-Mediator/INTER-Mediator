88b7e082149a6bab59ddbc5d958ce22a53ac192e
msyk <nii@msyk.net>
2014-04-03 21:00:29 +0900

    One practice is added, and some bug fixes.

---
+++ Samples/Practices/search_def.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * INTER-Mediator Ver.@@@@2@@@@ Released @@@@1@@@@
+ *
+ *   by Masayuki Nii  msyk@msyk.net Copyright (c) 2014 Masayuki Nii, All rights reserved.
+ *
+ *   This project started at the end of 2009.
+ *   INTER-Mediator is supplied under MIT License.
+ */
+require_once('../../INTER-Mediator.php');
+
+IM_Entry(
+    array(
+        array(
+            'name' => 'postalcode',
+            'records' => 10,
+            'maxrecords' => 10,
+            'paging' => true,
+        ),
+    ),
+    null,
+    array('db-class' => 'PDO'),
+    2
+);
