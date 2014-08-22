a2429bf21e061ea4ca80495bd58c7a026b6cc031
msyk <nii@msyk.net>
2014-08-22 16:20:57 +0900

    Sample_pdf was added.

---
+++ Samples/Sample_pdf/contexts_MySQL.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * INTER-Mediator Ver.@@@@2@@@@ Released @@@@1@@@@
+ *
+ *   by Masayuki Nii  msyk@msyk.net Copyright (c) 2010-2014 Masayuki Nii, All rights reserved.
+ *
+ *   This project started at the end of 2009.
+ *   INTER-Mediator is supplied under MIT License.
+ */
+require_once(dirname(__FILE__) . '/../../INTER-Mediator.php');
+
+IM_Entry(
+    array(
+        array(
+            'records' => 10,
+            'name' => 'productlist',
+            'view' => 'product',
+            'key' => 'id',
+            'query' => array(array('field' => 'name', 'value' => '%', 'operator' => 'LIKE')),
+            'sort' => array(array('field' => 'name', 'direction' => 'ASC'),),
+        ),
+        array(
+            'records' => 1,
+            'name' => 'productdetail',
+            'view' => 'product',
+            'key' => 'id',
+        ),
+    ),
+    array(
+        'media-context' => 'productlist',
+        'formatter' => array(
+            array('field' => 'product@unitprice', 'converter-class' => 'Number', 'parameter' => '0'),
+        ),
+    ),
+    array('db-class' => 'PDO'),
+    false
+);
