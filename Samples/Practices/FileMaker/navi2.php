35768662893f0770deb0f2eb1bcf2f4aed9f0831
msyk <nii@msyk.net>
2014-11-16 01:46:25 +0900

    Practice/Master-Detail improved.

---
+++ Samples/Practices/FileMaker/navi2.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * INTER-Mediator Ver.@@@@2@@@@ Released @@@@1@@@@
+ *
+ *   by Masayuki Nii  msyk@msyk.net Copyright (c) 2010-2014 Masayuki Nii, All rights reserved.
+ *
+ *   This project started at the end of 2009.
+ *   INTER-Mediator is supplied under MIT License.
+ */
+require_once(dirname(__FILE__) . '/../../../INTER-Mediator.php');
+
+IM_Entry(
+    array(
+        array(
+            'records' => 10,
+            'name' => 'productlist',
+            'view' => 'product',
+            'key' => 'id',
+            'sort' => array(array('field' => 'name', 'direction' => 'ASC'),),
+            'navi-control' => 'master',
+        ),
+        array(
+            'records' => 1,
+            'name' => 'productdetail',
+            'view' => 'product',
+            'table' => 'product',
+            'key' => 'id',
+            'navi-control' => 'detail-top',
+        ),
+    ),
+    array(
+        'formatter' => array(
+            array('field' => 'product@unitprice', 'converter-class' => 'Number', 'parameter' => '0'),
+        ),
+    ),
+    array('db-class' => 'FileMaker_FX'),
+    false
+);
