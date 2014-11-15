5347398c3c44df7470cd0b43c11aa72307af1e56
Atsushi Matsuo <famlog@gmail.com>
2014-11-16 01:19:10 +0900

    Add the "Practices" (Input Only) in the "Samples" directory for FileMaker Server

---
+++ Samples/Practices/FileMaker/survey_def.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * INTER-Mediator Ver.@@@@2@@@@ Released @@@@1@@@@
+ * 
+ *   by Masayuki Nii  msyk@msyk.net Copyright (c) 2010-2014 Masayuki Nii, All rights reserved.
+ * 
+ *   This project started at the end of 2009.
+ *   INTER-Mediator is supplied under MIT License.
+ */
+require_once(dirname(__FILE__) . '/../../../INTER-Mediator.php');
+
+IM_Entry(
+    array(
+        array(
+            'name' => 'surveyinput',
+            'table' => 'survey',
+            'key' => 'id',
+            'post-dismiss-message' => 'ありがとうございました',
+            'validation' => array(
+                array(
+                    'field' => 'Q1',
+                    'rule' => "value != ''",
+                    'message' => '何か入力してください',
+                    'notify' => 'inline'
+                ),
+            ),
+        ),
+        array(
+            'name' => 'surveylist',
+            'view' => 'survey',
+            'key' => 'id',
+            'records' => 10,
+            'maxrecords' => 10,
+            'paging' => true,
+            'sort' => array(
+                array('field'=>'Q2', 'direction' => 'DESC')
+            ),
+            'query' => array(
+          //      array('field' => 'Q2', 'operator'=>'>', 'value' => '43')
+            )
+        ),
+    ),
+    null,
+    array('db-class' => 'FileMaker_FX'),
+    false
+);
