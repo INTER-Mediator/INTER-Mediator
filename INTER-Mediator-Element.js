b550cebda304b0ba2627ce22145db347ecee13a0
Atsushi Matsuo <famlog@gmail.com>
2015-03-14 16:35:02 +0900

    Fix style code errors in INTER-Mediator-Element.js

--- INTER-Mediator-Element.js
+++ INTER-Mediator-Element.js
@@ -10,7 +10,7 @@
 var IMLibElement = {
     setValueToIMNode: function (element, curTarget, curVal, clearField) {
         var styleName, statement, currentValue, scriptNode, typeAttr, valueAttr, textNode,
-            needPostValueSet = false, nodeTag, curValues, i, patterns, formattedValue = null, 
+            needPostValueSet = false, nodeTag, curValues, i, patterns, formattedValue = null,
             formatSpec, formatOption, flags = {}, param1, mark;
         // IE should \r for textNode and <br> for innerHTML, Others is not required to convert
 
@@ -123,7 +123,7 @@ var IMLibElement = {
                         curVal = curVal.replace(/\r\n/g, "\r").replace(/\n/g, "\r");
                     }
                     element.appendChild(textNode);
-                } else if (curTarget.indexOf('style.') == 0) {
+                } else if (curTarget.indexOf('style.') === 0) {
                     styleName = curTarget.substring(6, curTarget.length);
                     element.style[styleName] = curVal;
                 } else {
@@ -146,7 +146,7 @@ var IMLibElement = {
                         curVal = curVal.replace(/\r\n/g, "\r").replace(/\n/g, "\r");
                     }
                     element.innerHTML = element.innerHTML.replace("$", curVal);
-                } else if (curTarget.indexOf('style.') == 0) {
+                } else if (curTarget.indexOf('style.') === 0) {
                     styleName = curTarget.substring(6, curTarget.length);
                     element.style[styleName] = curVal;
                 } else {
@@ -180,7 +180,7 @@ var IMLibElement = {
                         scriptNode.appendChild(textNode);
                         element.appendChild(scriptNode);
                     }
-                } else if (curTarget.indexOf('style.') == 0) {
+                } else if (curTarget.indexOf('style.') === 0) {
                     styleName = curTarget.substring(6, curTarget.length);
                     element.style[styleName] = curVal;
                 } else {
@@ -262,13 +262,13 @@ var IMLibElement = {
         } else {
             return "";
         }
-        if (INTERMediatorLib.isWidgetElement(element)
-            || (INTERMediatorLib.isWidgetElement(element.parentNode))) {
+        if (INTERMediatorLib.isWidgetElement(element) ||
+            (INTERMediatorLib.isWidgetElement(element.parentNode))) {
             newValue = element._im_getValue();
         } else if (nodeTag == "INPUT") {
             if (typeAttr == 'checkbox') {
                 dbspec = INTERMediatorOnPage.getDBSpecification();
-                if (dbspec["db-class"] != null && dbspec["db-class"] == "FileMaker_FX") {
+                if (dbspec["db-class"] !== null && dbspec["db-class"] == "FileMaker_FX") {
                     mergedValues = [];
                     targetNodes = element.parentNode.getElementsByTagName('INPUT');
                     for (k = 0; k < targetNodes.length; k++) {
@@ -318,14 +318,14 @@ var IMLibElement = {
         if (INTERMediator.ignoreOptimisticLocking) {
             return true;
         }
-        targetContext = contextInfo['context'];
-        targetField = contextInfo['field'];
-        keyingComp = contextInfo['record'].split('=');
+        targetContext = contextInfo.context;
+        targetField = contextInfo.field;
+        keyingComp = contextInfo.record.split('=');
         keyingField = keyingComp[0];
         keyingComp.shift();
         keyingValue = keyingComp.join('=');
         checkQueryParameter = {
-            name: contextInfo['context'].contextName,
+            name: contextInfo.context.contextName,
             records: 1,
             paging: false,
             fields: [targetField],
@@ -360,9 +360,9 @@ var IMLibElement = {
             if (currentVal.recordset && currentVal.recordset[0]) {
                 for (portalIndex in currentVal.recordset[0]) {
                     var portalRecord = currentVal.recordset[0][portalIndex];
-                    if (portalRecord[portalKey]
-                        && portalRecord[targetField] !== undefined
-                        && portalRecord[portalKey] == contextInfo.portal) {
+                    if (portalRecord[portalKey] &&
+                        portalRecord[targetField] !== undefined &&
+                        portalRecord[portalKey] == contextInfo.portal) {
                         currentFieldVal = portalRecord[targetField];
                         isCheckResult = true;
                     }
@@ -374,9 +374,9 @@ var IMLibElement = {
                 return false;
             }
         } else {
-            if (! currentVal.recordset
-                || ! currentVal.recordset[0]    // This value could be null or undefined
-                || currentVal.recordset[0][targetField] === undefined) {
+            if (! currentVal.recordset ||
+                ! currentVal.recordset[0] ||    // This value could be null or undefined
+                currentVal.recordset[0][targetField] === undefined) {
                 alert(INTERMediatorLib.getInsertedString(
                     INTERMediatorOnPage.getMessages()[1003], [targetField]));
                 return false;
@@ -389,7 +389,7 @@ var IMLibElement = {
             }
             currentFieldVal = currentVal.recordset[0][targetField];
         }
-        initialvalue = targetContext.getValue(contextInfo['record'], targetField, contextInfo.portal);
+        initialvalue = targetContext.getValue(contextInfo.record, targetField, contextInfo.portal);
 
         switch (element.tagName) {
             case "INPUT":
@@ -451,7 +451,8 @@ var IMLibElement = {
                         referes = {};
                         values = {};
                         for (j in calcObject.referes) {
-                            referes[j] = [], values[j] = [];
+                            referes[j] = [];
+                            values[j] = [];
                             for (k = 0; k < calcObject.referes[j].length; k++) {
                                 if (removeNodeId != calcObject.referes[j][k]) {
                                     referes[j].push(calcObject.referes[j][k]);
