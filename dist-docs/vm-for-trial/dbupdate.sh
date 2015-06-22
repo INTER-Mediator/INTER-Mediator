a770d5c9b85c32d892fb6feceee178cb08651638
Masayuki Nii <msyk@msyk.net>
2015-06-22 10:38:48 +0900

    dbupdate script modified

--- dist-docs/vm-for-trial/dbupdate.sh
+++ dist-docs/vm-for-trial/dbupdate.sh
@@ -11,11 +11,10 @@ IMDISTDOC="${IMROOT}/dist-docs"
 
 mysql -u root --password=im4135dev < "${IMDISTDOC}/sample_schema_mysql.txt"
 
-echo "im4135dev" | sudo -u postgres -S psql -c 'create database test_db;'
 echo "im4135dev" | sudo -u postgres -S psql -f "${IMDISTDOC}/sample_schema_pgsql.txt" test_db
 
-mkdir -p /var/db/im
-sqlite3 /var/db/im/sample.sq3 < "${IMDISTDOC}/sample_schema_sqlite.txt"
-chown -R www-data:im-developer /var/db/im
-chmod 775 /var/db/im
-chmod 664 /var/db/im/sample.sq3
+SQLITEDB="/var/db/im/sample.sq3"
+if [ -f "${SQLITEDB}" ]; then
+    rm "${SQLITEDB}"
+fi
+sqlite3  < "${IMDISTDOC}/sample_schema_sqlite.txt"
