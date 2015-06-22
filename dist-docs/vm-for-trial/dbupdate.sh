cedf6cdcbbe876f69db300bcc1c6b21ba4c4bd3a
Masayuki Nii <msyk@msyk.net>
2015-06-22 11:03:11 +0900

    dbupdate.sh modified

--- dist-docs/vm-for-trial/dbupdate.sh
+++ dist-docs/vm-for-trial/dbupdate.sh
@@ -11,10 +11,12 @@ IMDISTDOC="${IMROOT}/dist-docs"
 
 mysql -u root --password=im4135dev < "${IMDISTDOC}/sample_schema_mysql.txt"
 
+echo "im4135dev" | sudo -u postgres -S psql -c 'drop database if exists test_db;'
+echo "im4135dev" | sudo -u postgres -S psql -c 'create database test_db;'
 echo "im4135dev" | sudo -u postgres -S psql -f "${IMDISTDOC}/sample_schema_pgsql.txt" test_db
 
 SQLITEDB="/var/db/im/sample.sq3"
 if [ -f "${SQLITEDB}" ]; then
     rm "${SQLITEDB}"
 fi
-sqlite3  < "${IMDISTDOC}/sample_schema_sqlite.txt"
+sqlite3 "${SQLITEDB}" < "${IMDISTDOC}/sample_schema_sqlite.txt"
