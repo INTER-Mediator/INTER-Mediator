75b482c1f7fe2b3bb3b40c5214a4d6e1dacff7cf
Atsushi Matsuo <famlog@gmail.com>
2015-07-09 09:44:34 +0900

    Update deploy.sh etc. to fix ACL for INTER-Mediator-Server VM

--- dist-docs/vm-for-trial/recipe.rb
+++ dist-docs/vm-for-trial/recipe.rb
@@ -710,8 +710,8 @@ execute "sqlite3 /var/db/im/sample.sq3 < \"#{IMDISTDOC}/sample_schema_sqlite.txt
   command "sqlite3 /var/db/im/sample.sq3 < \"#{IMDISTDOC}/sample_schema_sqlite.txt\""
 end
 
-execute "setfacl --recursive --modify g:im-developer:rw,d:g:im-developer:rw \"#{WEBROOT}\"" do
-  command "setfacl --recursive --modify g:im-developer:rw,d:g:im-developer:rw \"#{WEBROOT}\""
+execute "setfacl --recursive --modify g:im-developer:rwx,d:g:im-developer:rwx \"#{WEBROOT}\"" do
+  command "setfacl --recursive --modify g:im-developer:rwx,d:g:im-developer:rwx \"#{WEBROOT}\""
 end
 
 execute "chown -R developer:im-developer \"#{WEBROOT}\"" do
