1e3e0a3ff25e32f3689c35884977560be7739e91
Atsushi Matsuo <famlog@gmail.com>
2015-07-31 06:30:09 +0900

    Preserve the value of INTERMediator.startFrom after inserting a new record for a list page only

--- INTER-Mediator-Navi.js
+++ INTER-Mediator-Navi.js
@@ -286,17 +286,19 @@ IMLibPageNavigation = {
 
         if (newId > -1) {
             restore = INTERMediator.additionalCondition;
-            //INTERMediator.startFrom = 0;
             if (contextDef.records <= 1) {
+                INTERMediator.startFrom = 0;
+                INTERMediator.pagedAllCount = 1;
                 conditions = INTERMediator.additionalCondition;
                 conditions[targetName] = {field: keyField, value: newId};
                 INTERMediator.additionalCondition = conditions;
                 IMLibLocalContext.archive();
+            } else {
+                INTERMediator.pagedAllCount++;
             }
             INTERMediator_DBAdapter.unregister();
             INTERMediator.constructMain(true);
             INTERMediator.additionalCondition = restore;
-            INTERMediator.pagedAllCount++;
             IMLibPageNavigation.navigationSetup();
         }
         IMLibCalc.recalculation();
