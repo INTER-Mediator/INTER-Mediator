3cacc67043dd7b869359801fd0b3f98047eb3c02
Atsushi Matsuo <famlog@gmail.com>
2015-08-17 02:41:54 +0900

    Modify defedit_Test.php and pageedit_Test.php

--- INTER-Mediator-UnitTest/pageedit_Test.php
+++ INTER-Mediator-UnitTest/pageedit_Test.php
@@ -2,9 +2,6 @@
 /**
  * pageedit_Test file
  */
-require_once(dirname(__FILE__) . '/../INTER-Mediator.php');
-require_once(dirname(__FILE__) . '/../GenerateJSCode.php');
-require_once(dirname(__FILE__) . '/../INTER-Mediator-Support/pageedit.php');
 
 class pageedit_Test extends PHPUnit_Framework_TestCase
 {
@@ -20,6 +17,7 @@ class pageedit_Test extends PHPUnit_Framework_TestCase
     public function test___construct()
     {
         ob_start();
+        require_once(dirname(__FILE__) . '/../INTER-Mediator-Support/pageedit.php');
         $output = ob_get_contents();
         $this->assertNotContains('INTERMediator.debugMode=', $output);
         ob_clean();
