767bff1740df732c22a46946d3b7afaad27404b7
Masayuki Nii <msyk@msyk.net>
2015-08-24 20:43:20 +0900

    getNodeIdsFromIMDefinition supports array.

--- INTER-Mediator-Page.js
+++ INTER-Mediator-Page.js
@@ -705,7 +705,7 @@ INTERMediatorOnPage = {
 
     getNodeIdsFromIMDefinition: function (imDefinition, fromNode, justFromNode) {
         "use strict";
-        var enclosureNode, nodeIds, nodeDefs;
+        var enclosureNode, nodeIds, i;
 
         if (justFromNode === true) {
             enclosureNode = fromNode;
@@ -716,7 +716,13 @@ INTERMediatorOnPage = {
         }
         if (enclosureNode != null) {
             nodeIds = [];
-            seekNode(enclosureNode, imDefinition);
+            if (Array.isArray(enclosureNode))   {
+                for (i = 0 ; i < enclosureNode.length ; i++)    {
+                    seekNode(enclosureNode[i], imDefinition);
+                }
+            } else {
+                seekNode(enclosureNode, imDefinition);
+            }
         }
         return nodeIds;
 
