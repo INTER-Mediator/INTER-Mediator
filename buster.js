d413cf135755d03154256b7e6be2d47e04eb7b11
Masayuki Nii <msyk@msyk.net>
2015-09-13 20:38:42 +0900

    JS test configuration improved.

--- buster.js
+++ buster.js
@@ -3,9 +3,12 @@ var config = module.exports;
 config["My tests"] = {
     environment: "browser",
     sources: [
-        "*.js",
+        "INTER-Mediator.js",
+        "INTER-Mediator-Context.js",
+        "INTER-Mediator-Lib.js",
         "lib/js_lib/*.js",
-        "lib/bi2php/*.js"
+        "lib/bi2php/*.js",
+        "*.js"
     ],
     tests: [
         "INTER-Mediator-UnitTest/*-test.js"
