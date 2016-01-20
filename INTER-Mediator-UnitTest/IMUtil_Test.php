c01eefc8041bb0d06b4259803676e436b9f7f1ee
Atsushi Matsuo <famlog@gmail.com>
2016-01-21 00:45:01 +0900

    Fix protecting CSRF and allow multiple values in $webServerName

--- INTER-Mediator-UnitTest/IMUtil_Test.php
+++ INTER-Mediator-UnitTest/IMUtil_Test.php
@@ -17,4 +17,70 @@ class IMUtil_Test extends PHPUnit_Framework_TestCase {
         $str = $this->util->removeNull("INTER\x00-Mediator");
         $this->assertEquals($str, "INTER-Mediator");
     }
+
+    public function test_getFromParamsPHPFile()
+    {
+        $result = $this->util->getFromParamsPHPFile(array('webServerName'), true);
+        $this->assertNull($result['webServerName']);
+    }
+
+    public function test_protectCSRF()
+    {
+        $result = $this->util->protectCSRF();
+        $this->assertFalse($result);
+
+        $_SERVER = array();
+        $_SERVER['HTTP_HOST'] = '192.168.56.101';
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+        $_SERVER['HTTP_X_FROM'] = 'http://192.168.56.101/';
+        $result = $this->util->protectCSRF();
+        $this->assertTrue($result);
+
+        $_SERVER['HTTP_ORIGIN'] = 'https://192.168.56.101/';
+        $result = $this->util->protectCSRF();
+        $this->assertFalse($result);
+
+        $_SERVER['HTTP_ORIGIN'] = 'http://192.168.56.101/';
+        $result = $this->util->protectCSRF();
+        $this->assertTrue($result);
+
+        $_SERVER['HTTP_ORIGIN'] = 'http://192.168.56.101:80/';
+        $result = $this->util->protectCSRF();
+        $this->assertTrue($result);
+    }
+
+    public function test_checkHost()
+    {
+        if (((float)phpversion()) >= 5.3) {
+            $this->reflectionMethod = new ReflectionMethod('IMUtil', 'checkHost');
+            $this->reflectionMethod->setAccessible(true);
+
+            $result = $this->reflectionMethod->invokeArgs($this->util, array('www.inter-mediator.com', 'www.inter-mediator.com'));
+            $this->assertTrue($result);
+
+            $result = $this->reflectionMethod->invokeArgs($this->util, array('www.inter-mediator.com', 'inter-mediator.com'));
+            $this->assertTrue($result);
+
+            $result = $this->reflectionMethod->invokeArgs($this->util, array('WWW.inter-mediator.com', 'inter-mediator.com'));
+            $this->assertTrue($result);
+
+            $result = $this->reflectionMethod->invokeArgs($this->util, array('inter-mediator.com', 'inter-mediator.com'));
+            $this->assertTrue($result);
+
+            $_SERVER = array();
+            $_SERVER['SERVER_ADDR'] = '192.168.56.101';
+            $result = $this->reflectionMethod->invokeArgs($this->util, array('192.168.56.101', $_SERVER['SERVER_ADDR']));
+            $this->assertTrue($result);
+
+            $result = $this->reflectionMethod->invokeArgs($this->util, array('www.inter-mediator.com', ''));
+            $this->assertFalse($result);
+
+            $result = $this->reflectionMethod->invokeArgs($this->util, array('www.inter-mediator.com', 'ww.inter-mediator.com'));
+            $this->assertFalse($result);
+
+            $_SERVER = array();
+            $result = $this->reflectionMethod->invokeArgs($this->util, array('192.168.56.101', '56.101'));
+            $this->assertFalse($result);
+        }
+    }
 }
--- INTER-Mediator.php
+++ INTER-Mediator.php
@@ -119,27 +119,8 @@ function IM_Entry($datasource, $options, $dbspecification, $debug = false)
     } else {
         $dbInstance = new DB_Proxy();
         $dbInstance->initialize($datasource, $options, $dbspecification, $debug);
-        /*
-         * Prevent CSRF Attack with XMLHttpRequest
-         * http://d.hatena.ne.jp/hasegawayosuke/20130302/p1
-         */
-        $params = IMUtil::getFromParamsPHPFile(array('webServerName'), true);
-        $webServerName = $params['webServerName'];
-        if (isset($_SERVER['HTTP_X_FROM'])) {
-            $from = parse_url($_SERVER['HTTP_X_FROM']);
-        }
-        if (isset($_SERVER['HTTP_ORIGIN'])) {
-            $origin = parse_url($_SERVER['HTTP_ORIGIN']);
-        }
-        if (isset($_SERVER['HTTP_HOST']) &&
-            (is_null($webServerName) ||
-                (!is_null($webServerName && $_SERVER['HTTP_HOST'] === $webServerName))) &&
-            isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
-            $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest' &&
-            isset($_SERVER['HTTP_X_FROM']) &&
-            (!isset($_SERVER['HTTP_ORIGIN']) ||
-                $from['scheme'] . '://' . $from['host'] . ':' . $from['port'] ===
-                $origin['scheme'] . '://' . $origin['host'] . ':' . $origin['port'])) {
+        $util = new IMUtil();
+        if ($util->protectCSRF() === TRUE) {
             $dbInstance->processingRequest($options);
             $dbInstance->finishCommunication(false);
         } else {
