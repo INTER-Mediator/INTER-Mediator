2dfb07921822a3599279e35615c51a87fd694d63
Atsushi Matsuo <famlog@gmail.com>
2016-09-22 11:33:31 +0900

    Update buster.js to disable test cases of queuing (INTER-Mediator-Queuing-test.js) temporally

--- spec/buster.js
+++ spec/buster.js
@@ -4,14 +4,17 @@ config["My tests"] = {
     environment: "browser",
     rootPath: "../",
     sources: [
-        "INTER-Mediator.js",
-        "INTER-Mediator-Context.js",
-        "INTER-Mediator-Lib.js",
+        "*.js",
         "lib/js_lib/*.js",
-        "lib/bi2php/*.js",
-        "*.js"
+        "lib/bi2php/*.js"
     ],
     tests: [
-        "INTER-Mediator-UnitTest/*-test.js"
+        "INTER-Mediator-UnitTest/INTER-Mediator-Context-test.js",
+        "INTER-Mediator-UnitTest/INTER-Mediator-Element-test.js",
+        "INTER-Mediator-UnitTest/INTER-Mediator-Lib-test.js",
+        "INTER-Mediator-UnitTest/INTER-Mediator-Page-test.js",
+        "INTER-Mediator-UnitTest/RSA_JavaScript-test.js",
+        "INTER-Mediator-UnitTest/js-expression-eval-test.js",
+        "INTER-Mediator-UnitTest/sha1-test.js"
     ]
 };
