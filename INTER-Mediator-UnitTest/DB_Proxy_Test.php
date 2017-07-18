3e239eb4edb3fb5b0a68fbec07ca637ceb9e57ba
Atsushi Matsuo <famlog@gmail.com>
2017-07-18 22:41:09 +0900

    Update DB_PDO_Test_Common.php and DB_Proxy_Test.php

--- INTER-Mediator-UnitTest/DB_Proxy_Test.php
+++ INTER-Mediator-UnitTest/DB_Proxy_Test.php
@@ -7,6 +7,7 @@ require_once(dirname(__FILE__) . '/../DB_Settings.php');
 require_once(dirname(__FILE__) . '/../DB_UseSharedObjects.php');
 require_once(dirname(__FILE__) . '/../DB_Proxy.php');
 require_once(dirname(__FILE__) . '/../DB_Formatters.php');
+require_once(dirname(__FILE__) . '/../DB_Support/DB_Auth_Common.php');
 
 class DB_Proxy_Test extends PHPUnit_Framework_TestCase
 {
