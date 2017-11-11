b037089c9dbd94f9fc8cac0f444a32d510d9a438
Atsushi Matsuo <famlog@gmail.com>
2017-11-11 12:53:29 +0900

    Update samples_spec.rb

--- dist-docs/docker/spec/samples/samples_spec.rb
+++ dist-docs/docker/spec/samples/samples_spec.rb
@@ -3,7 +3,7 @@
 #   - Install gem of rspec and selenium-webdriver on the host of VM ("gem install rspec selenium-webdriver")
 #   - Install Firefox and geckodriver (Chrome and chromedriver) on the host of VM
 #   - Change directory to the root directory of INTER-Mediator on the host of VM
-#   - Run "FQDN=192.168.56.101 rspec --default-path=dist-docs/docker/spec -f doc -c dist-docs/docker/spec/samples/samples_spec.rb" on the host of VM
+#   - Run "ADDR=192.168.56.101 rspec --default-path=dist-docs/docker/spec -f doc -c dist-docs/docker/spec/samples/samples_spec.rb" on the host of VM
 
 require "selenium-webdriver"
 
@@ -19,9 +19,9 @@ describe "INTER-Mediator-Server VM" do
       @port = ENV['PORT']
     end
     @port = @port.to_s
-    @fqdn = '127.0.0.1'
-    if ENV['FQDN'] != nil
-      @fqdn = ENV['FQDN']
+    @addr = '127.0.0.1'
+    if ENV['ADDR'] != nil
+      @addr = ENV['ADDR']
     end
     @browser = 'firefox'
     if ENV['BROWSER'] != nil
@@ -32,7 +32,7 @@ describe "INTER-Mediator-Server VM" do
     elsif @browser == 'chrome'
       @driver = Selenium::WebDriver.for :chrome
     end
-    @driver.navigate.to "http://" + @fqdn + "/"
+    @driver.navigate.to "http://" + @addr + "/"
     @wait = Selenium::WebDriver::Wait.new(:timeout => 15)
   end
 
@@ -42,17 +42,17 @@ describe "INTER-Mediator-Server VM" do
 
   it "The path of 'Sample Program' should be '/INTER-Mediator/Samples/'." do
     element = @driver.find_element(:xpath, "//a[contains(@href, 'Samples')]")
-    expect(element.attribute("href")).to eq("http://" + @fqdn + "/INTER-Mediator/Samples/")
+    expect(element.attribute("href")).to eq("http://" + @addr + "/INTER-Mediator/Samples/")
     @driver.navigate.to element.attribute("href")
     expect(@driver.title).to eq("INTER-Mediator - Samples")
   end
 
   it "Practice 'search(no JavaScript)' for MySQL/MariaDB should be working" do
-    @driver.navigate.to "http://" + @fqdn + "/INTER-Mediator/Samples/"
+    @driver.navigate.to "http://" + @addr + "/INTER-Mediator/Samples/"
     @wait.until {
       element = @driver.find_element(:xpath, "//a[contains(@href, 'Practices/search_page1.html')]")
       element.click
-      #@driver.navigate.to "http://" + @fqdn + "/INTER-Mediator/Samples/Practices/search_page1.html"
+      #@driver.navigate.to "http://" + @addr + "/INTER-Mediator/Samples/Practices/search_page1.html"
       sleep 2
       elements = @driver.find_elements(:xpath, "//div[@data-im='postalcode@f3']")
       expect(elements[0].text).to eq("1000000")
@@ -65,11 +65,11 @@ describe "INTER-Mediator-Server VM" do
   end
 
   it "Practice 'search(using JavaScript)' for MySQL/MariaDB should be working" do
-    @driver.navigate.to "http://" + @fqdn + "/INTER-Mediator/Samples/"
+    @driver.navigate.to "http://" + @addr + "/INTER-Mediator/Samples/"
     @wait.until {
       element = @driver.find_element(:xpath, "//a[contains(@href, 'Practices/search_page2.html')]")
       element.click
-      #@driver.navigate.to "http://" + @fqdn + "/INTER-Mediator/Samples/Practices/search_page2.html"
+      #@driver.navigate.to "http://" + @addr + "/INTER-Mediator/Samples/Practices/search_page2.html"
       sleep 2
       elements = @driver.find_elements(:xpath, "//div[@data-im='postalcode@f3']")
       expect(elements[0].text).to eq("1000000")
