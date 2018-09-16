adad4c8278c47313575a282b849561a0d1ab4a3d
Masayuki Nii <msyk@msyk.net>
2018-09-16 21:17:23 +0900

    No condition.

--- spec/INTER-Mediator-UnitTest/DB_PDO_Test_Common.php
+++ spec/INTER-Mediator-UnitTest/DB_PDO_Test_Common.php
@@ -657,7 +657,7 @@ abstract class DB_PDO_Test_Common extends TestCase
         $this->assertEquals(count($result), 1, "Just 1 records should be retrieved.");
 
         $this->dbProxySetupForAccess("person", 1);
-        $this->db_proxy->dbSettings->addExtraCriteria("name", "IS NOT NULL", "");
+        //$this->db_proxy->dbSettings->addExtraCriteria("name", "IS NOT NULL", "");
         $result = $this->db_proxy->readFromDB();
         var_export($this->db_proxy->logger->getErrorMessages());
         var_export($this->db_proxy->logger->getDebugMessages());
