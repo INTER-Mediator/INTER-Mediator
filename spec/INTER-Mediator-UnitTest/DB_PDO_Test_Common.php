b4a815adf55e7947eb589894d92bbd50561b14a7
Masayuki Nii <msyk@msyk.net>
2018-09-16 23:53:46 +0900

    Modify unit test.

--- spec/INTER-Mediator-UnitTest/DB_PDO_Test_Common.php
+++ spec/INTER-Mediator-UnitTest/DB_PDO_Test_Common.php
@@ -653,11 +653,10 @@ abstract class DB_PDO_Test_Common extends TestCase
         $this->dbProxySetupForAccess("person", 1);
         $result = $this->db_proxy->readFromDB();
         $aName = $result[0]['name'];
-        var_dump($result);
         $this->assertEquals(count($result), 1, "Just 1 records should be retrieved.");
 
         $this->dbProxySetupForAccess("person", 1);
-        //$this->db_proxy->dbSettings->addExtraCriteria("name", "IS NOT NULL", "");
+        $this->db_proxy->dbSettings->addExtraCriteria("address", "IS NOT NULL", "");
         $result = $this->db_proxy->readFromDB();
         var_export($this->db_proxy->logger->getErrorMessages());
         var_export($this->db_proxy->logger->getDebugMessages());
