6e71cd8609dd4eae46c6e47adbb3a51a17b565c6
Atsushi Matsuo <famlog@gmail.com>
2018-10-25 15:55:24 +0900

    Fix INTERMediator.isIE property to detect Internet Explorer 11 (Change false to true if using IE11)

--- INTER-Mediator.js
+++ INTER-Mediator.js
@@ -238,6 +238,9 @@ var INTERMediator = {
           c = ua.charAt(i);
           if (!(c === ' ' || c === '.' || (c >= '0' && c <= '9'))) {
             INTERMediator.ieVersion = INTERMediatorLib.toNumber(ua.substring(position + 10, i)) + 4;
+            if (INTERMediator.ieVersion === 11) {
+              INTERMediator.isIE = true
+            }
             break;
           }
         }
