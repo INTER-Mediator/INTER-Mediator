133579b63da7702fad21d1ee3f8091976c784e6c
msyk <nii@msyk.net>
2018-12-29 21:16:23 +0900

    Removing out-dated records from issuedhash table improved

--- DB_Support/DB_Auth_Handler_PDO.php
+++ DB_Support/DB_Auth_Handler_PDO.php
@@ -170,18 +170,20 @@ class DB_Auth_Handler_PDO extends DB_Auth_Common implements Auth_Interface_DB
         if ($hashTable == null) {
             return false;
         }
-
         if (!$this->dbClass->setupConnection()) { //Establish the connection
             return false;
         }
-        $currentDTStr = $this->dbClass->link->quote(IMUtil::currentDTString($this->dbSettings->getExpiringSeconds()));
-        $sql = "delete from {$hashTable} where clienthost IS NOT NULL AND expired < {$currentDTStr}";
-        $this->logger->setDebugMessage("[authSupportRemoveOutdatedChallenges] {$sql}");
+        $expireSeconds = $this->dbSettings->getExpiringSeconds();
+        $currentDTStr = $this->dbClass->link->quote(IMUtil::currentDTString($expireSeconds));
+        $longBeforeDTStr = $this->dbClass->link->quote(IMUtil::currentDTString(3600 * 24 * 3));
+        $sql = "{$this->dbClass->handler->sqlDELETECommand()}{$hashTable} WHERE".
+            " (clienthost IS NOT NULL AND expired < {$currentDTStr}) OR (expired < {$longBeforeDTStr})";
         $result = $this->dbClass->link->query($sql);
         if ($result === false) {
             $this->dbClass->errorMessageStore('Select:' . $sql);
             return false;
         }
+        $this->logger->setDebugMessage("[authSupportRemoveOutdatedChallenges] {$sql}");
         return true;
     }
 
@@ -817,6 +819,11 @@ class DB_Auth_Handler_PDO extends DB_Auth_Common implements Auth_Interface_DB
         if ($userTable == null) {
             return false;
         }
+        $hashTable = $this->dbSettings->getgetHashTable();
+        if ($hashTable == null) {
+            return false;
+        }
+
         if (!$this->dbClass->setupConnection()) { //Establish the connection
             return false;
         }
@@ -826,6 +833,15 @@ class DB_Auth_Handler_PDO extends DB_Auth_Common implements Auth_Interface_DB
         $result = $this->dbClass->link->query($sql);
         if ($result === false) {
             $this->dbClass->errorMessageStore('Update:' . $sql);
+            return false;
+        }
+        $this->logger->setDebugMessage("[authSupportUserEnrollmentActivateUser] {$sql}");
+
+        $sql = "{$this->dbClass->handler->sqlDELETECommand()}{$hashTable} " .
+            " WHERE user_id=" . $this->dbClass->link->quote($userID);
+        $result = $this->dbClass->link->query($sql);
+        if ($result === false) {
+            $this->dbClass->errorMessageStore('Delete:' . $sql);
             return false;
         }
         $this->logger->setDebugMessage("[authSupportUserEnrollmentActivateUser] {$sql}");
