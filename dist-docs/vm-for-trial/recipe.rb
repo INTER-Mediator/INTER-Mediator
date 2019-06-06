b14ef479eafa94d5176b740b256fea8ab8ba386f
Atsushi Matsuo <famlog@gmail.com>
2019-06-07 03:01:21 +0900

    Update recipe.rb for CI

--- dist-docs/vm-for-trial/recipe.rb
+++ dist-docs/vm-for-trial/recipe.rb
@@ -1910,11 +1910,11 @@ elsif node[:platform] == 'ubuntu'
     execute 'gem2.3 install bundler --no-ri --no-rdoc' do
       command 'gem2.3 install bundler --no-ri --no-rdoc'
     end
-    execute 'gem2.3 install ffi -v "1.9.18" --no-ri --no-rdoc' do
-      command 'gem2.3 install ffi -v "1.9.18" --no-ri --no-rdoc'
+    execute 'gem2.3 install ffi -v --no-doc' do
+      command 'gem2.3 install ffi -v --no-doc'
     end
-    execute 'gem2.3 install selenium-webdriver --no-ri --no-rdoc' do
-      command 'gem2.3 install selenium-webdriver --no-ri --no-rdoc'
+    execute 'gem2.3 install selenium-webdriver -v "3.142.3" --no-doc' do
+      command 'gem2.3 install selenium-webdriver -v "3.142.3" --no-doc'
     end
   else
     package 'ruby' do
