8074c38d8ef817bf75c41a2ab7466f4cc29f97c3
msyk <nii@msyk.net>
2019-10-25 17:38:44 +0900

    Messages from INTER-Mediator can be altered.

--- params.php
+++ params.php
@@ -239,3 +239,7 @@ $serviceServerHost = "localhost";
 $stopSSEveryQuit = false;
 $bootWithInstalledNode = false;
 $preventSSAutoBoot = false;
+
+// Altering messages, overwrite and/or adding new messages.
+$messages['default'][1022] = "We don't support Internet Explorer. We'd like you to access by Edge or any major browser.";
+$messages['ja'][1022] = "Internet Explorerは使用できません。Edgeあるいは他の一般的なブラウザをご利用ください。";
