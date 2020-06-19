8087065bba9f7ea23ee8e796bc8ab4296d3d71f9
msyk <nii@msyk.net>
2020-06-19 11:58:41 +0900

    Add a line 'error_reporting(E_ALL);' at the start of unit tests.

--- spec/INTER-Mediator-UnitTest/INTERMediator_AllTests.php
+++ spec/INTER-Mediator-UnitTest/INTERMediator_AllTests.php
@@ -13,6 +13,8 @@
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
+error_reporting(E_ALL);
+
 use \PHPUnit\Framework\TestCase;
 use \PHPUnit\Framework\TestSuite;
 
