91f306628057f0bb4e1e359e2799300c5b596fb6
Atsushi Matsuo <famlog@gmail.com>
2020-06-21 10:26:40 +0900

    Update dbupdate.sh and recipe.rb

--- dist-docs/vm-for-trial/spec/192.168.56.101/sample_spec.rb
+++ dist-docs/vm-for-trial/spec/192.168.56.101/sample_spec.rb
@@ -865,7 +865,7 @@ describe file('/var/www/localhost/htdocs/INTER-Mediator/node_modules/jest/bin/je
   it { should be_mode 755 }
 end
 
-describe file('/etc/motd'), :if => os[:family] == 'alpine' do
+describe file('/etc/motd'), :if => os[:family] == 'redhat' || os[:family] == 'alpine' do
   its(:content) { should match /Welcome to INTER-Mediator-Server VM!/ }
 end
 
