ad09e02d1ac867d58095ef70bcbd8af537e800ba
Atsushi Matsuo <famlog@gmail.com>
2021-02-20 21:26:33 +0900

    Update config.yml for CircleCI

--- dist-docs/docker/spec/samples/samples_spec.rb
+++ dist-docs/docker/spec/samples/samples_spec.rb
@@ -38,6 +38,9 @@ describe "INTER-Mediator-Server VM" do
       options = Selenium::WebDriver::Chrome::Options.new
       options.add_preference('intl.accept_languages', 'en-US, en')
       options.add_preference('general.useragent.locale', 'en-US')
+      options.add_argument('--headless')
+      options.add_argument('--disable-gpu')
+      options.add_argument('--window-size=1280,800')
       @driver = Selenium::WebDriver.for :chrome, options: options
     end
     @driver.navigate.to "http://" + @addr + "/"
@@ -45,8 +48,8 @@ describe "INTER-Mediator-Server VM" do
     @driver.manage.timeouts.page_load = 10
   end
 
-  it "The title of the first page should be 'INTER-Mediator 6-dev - VM for Trial'." do
-    expect(@driver.title).to eq("INTER-Mediator 6-dev - VM for Trial")
+  it "The title of the first page should be 'INTER-Mediator 7 - VM for Trial'." do
+    expect(@driver.title).to eq("INTER-Mediator 7 - VM for Trial")
   end
 
   it "Page File Editor should be working" do
