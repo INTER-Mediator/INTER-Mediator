7d14165bb37821d6b3db2b055959a279cb1ff02b
Atsushi Matsuo <famlog@gmail.com>
2021-05-23 17:38:48 +0900

    Support handling Claris FileMaker's checkbox set feature on Post Only mode

--- src/php/DB/FileMaker_DataAPI.php
+++ src/php/DB/FileMaker_DataAPI.php
@@ -1,16 +1,16 @@
 <?php
 /**
  * INTER-Mediator
- * Copyright (c) INTER-Mediator Directive Committee (http://inter-mediator.org)
+ * Copyright (c) INTER-Mediator Directive Committee (https://inter-mediator.org)
  * This project started at the end of 2009 by Masayuki Nii msyk@msyk.net.
  *
  * INTER-Mediator is supplied under MIT License.
  * Please see the full license for details:
  * https://github.com/INTER-Mediator/INTER-Mediator/blob/master/dist-docs/License.txt
  *
- * @copyright     Copyright (c) INTER-Mediator Directive Committee (http://inter-mediator.org)
+ * @copyright     Copyright (c) INTER-Mediator Directive Committee (https://inter-mediator.org)
  * @link          https://inter-mediator.com/
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @license       https://www.opensource.org/licenses/mit-license.php MIT License
  */
 
 namespace INTERMediator\DB;
@@ -1035,6 +1035,11 @@ class FileMaker_DataAPI extends UseSharedObjects implements DBClass_Interface
             $field = $requiredFields[$i];
             $value = $fieldValues[$i];
             if ($field != $keyFieldName) {
+                if (isset($recordData[$field]) && !empty($recordData[$field])) {
+                    // for handling checkbox on Post Only mode
+                    $value = $recordData[$field] . "\r" . $value;
+                    unset($recordData[$field]);
+                }
                 $recordData += array(
                     $field =>
                         $this->formatter->formatterToDB(
