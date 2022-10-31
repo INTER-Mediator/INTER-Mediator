c993dd52c35d2f364f15f0feaf46ae8d230174d5
msyk <nii@msyk.net>
2022-10-31 18:13:22 +0900

    Email by Symphony mailer.

---
+++ samples/Sample_form/mail_test.js
@@ -0,0 +1,28 @@
+INTERMediatorOnPage.doBeforeConstruct = function () {
+  INTERMediatorLog.suppressDebugMessageOnPage = true
+}
+
+INTERMediatorOnPage.doAfterConstruct = function () {
+  document.getElementById('wrapper').style.display = 'block'
+}
+
+function sendMail(num, pid) {
+  IMLibQueue.setTask((complete) => {
+    INTERMediator_DBAdapter.db_query_async(
+      {
+        name: `mail_send_${num}`,
+        records: 1,
+        useoffset: false,
+        conditions: [{field: "id", operator: "=", value: pid}]
+      },
+      (result) => {
+        INTERMediatorLog.flushMessage()
+        complete()
+      },
+      () => {
+        INTERMediatorLog.flushMessage()
+        complete()
+      }
+    )
+  })
+}
