79bf18a88326a6edc0686d2d1fa41bd72472a009
Masayuki Nii <nii@msyk.net>
2023-12-24 13:39:48 +0900

    e2e-test modified.

--- spec/run/wdio-chrome.conf.js
+++ spec/run/wdio-chrome.conf.js
@@ -37,6 +37,9 @@ exports.config = {
     // './test/specs/search_page_mysql.e2e.js',
     // './test/specs/search_page_postgresql.e2e.js',
     // './test/specs/search_page_sqlite.e2e.js',
+    './test/specs/sync_mysql.e2e.js',
+    // './test/specs/sync_postgresql.e2e.js',
+    //'./test/specs/sync_sqlite.e2e.js',
   ],
   // Patterns to exclude.
   exclude: [],
