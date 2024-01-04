fb57d8b0b487d8745ca9e8e8ed541c31903f3ce8
Masayuki Nii <nii@msyk.net>
2024-01-05 08:29:38 +0900

    Remove the calc and lookup e2e-test of Firefox from GitHub Actions.

--- spec/run/wdio-firefox.conf.js
+++ spec/run/wdio-firefox.conf.js
@@ -37,9 +37,9 @@ exports.config = {
     // './test/specs/search_page_mysql.e2e.js',
     // './test/specs/search_page_postgresql.e2e.js',
     // './test/specs/search_page_sqlite.e2e.js',
-    './test/specs/calc_lookup_page_mysql.e2e.js',
-    './test/specs/calc_lookup_page_postgresql.e2e.js',
-    './test/specs/calc_lookup_page_sqlite.e2e.js',
+    // './test/specs/calc_lookup_page_mysql.e2e.js',
+    // './test/specs/calc_lookup_page_postgresql.e2e.js',
+    // './test/specs/calc_lookup_page_sqlite.e2e.js',
   ],
   // Patterns to exclude.
   exclude: [],
@@ -59,7 +59,7 @@ exports.config = {
   // and 30 processes will get spawned. The property handles how many capabilities
   // from the same test should run tests.
   //
-  maxInstances: 1,
+  maxInstances: 3,
   //
   // If you have trouble getting all important capabilities together, check out the
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -70,7 +70,7 @@ exports.config = {
     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
     // grid with only 5 firefox instances available you can make sure that not more than
     // 5 instances get started at a time.
-    maxInstances: 1, // This is prefer for setting to 1 here
+    maxInstances: 3, // This is prefer for setting to 1 here
     //
     browserName: 'firefox',
     'moz:firefoxOptions': {
