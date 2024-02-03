3fee6548752852ab7099224696ad6868f95e75fb
Masayuki Nii <nii@msyk.net>
2024-02-03 19:17:38 +0900

    End-to-End tests updated.

--- spec/run/wdio-edge.conf.js
+++ spec/run/wdio-edge.conf.js
@@ -70,7 +70,7 @@ exports.config = {
     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
     // grid with only 5 firefox instances available you can make sure that not more than
     // 5 instances get started at a time.
-    maxInstances: 1,
+    maxInstances: 1, // If other than 1, Master-Detail Page's #3 test fails
     //
     browserName: 'MicrosoftEdge',
     // 'ms:edgeOptions': {
