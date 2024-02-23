4f0807bce778039331ac272466feb3af36607abb
Masayuki Nii <nii@msyk.net>
2024-02-23 09:19:18 +0900

    Lock file for composer generated on PHP@7.4, and dist-docs/installfiles.sh script modified.

--- dist-docs/installfiles.sh
+++ dist-docs/installfiles.sh
@@ -4,6 +4,11 @@ distDocDir=$(cd $(dirname "$0"); pwd)
 originalPath=$(dirname "${distDocDir}")
 cd "${originalPath}"
 
+brew unlink php
+brew link php@7.4
+
+rm -rf vendor node_modules
+
 mv '__Did_you_run_composer_update.txt' spec/tempfile
 composer update --with-all-dependencies
 mv spec/tempfile '__Did_you_run_composer_update.txt'
@@ -13,3 +18,6 @@ npm update
 
 cd ../run-safari
 npm update
+
+brew unlink php@7.4
+brew link php
