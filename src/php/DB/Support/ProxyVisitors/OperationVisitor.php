026a47e3168024e000d30011183f3c0ee4b02175
Atsushi Matsuo <famlog@gmail.com>
2024-08-04 01:33:31 +0900

    Revert OperationVisitor.php and update phpstan.yml for CI

--- src/php/DB/Support/ProxyVisitors/OperationVisitor.php
+++ src/php/DB/Support/ProxyVisitors/OperationVisitor.php
@@ -35,7 +35,7 @@ abstract class OperationVisitor
 
     /**
      * @param OperationElement $e
-     * @return bool
+     * @return void
      */
     abstract public function visitIsAuthAccessing(OperationElement $e): bool;
 
