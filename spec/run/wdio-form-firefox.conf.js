35ee98a60f1f0baf4612b3a8f5b3fe80fc56f651
Masayuki Nii <nii@msyk.net>
2025-05-18 13:41:26 +0900

    Modified any places.

--- spec/run/wdio-form-firefox.conf.js
+++ spec/run/wdio-form-firefox.conf.js
@@ -72,6 +72,9 @@ exports.config = {
     maxInstances: 3, //
     //
     browserName: 'firefox',
+    'moz:firefoxOptions': {
+      args: ['-headless']
+    }
   }
     // If outputDir is provided WebdriverIO can capture driver session logs
     // it is possible to configure which logTypes to include/exclude.
