70137b9075801f350663a5b4e0bf82d2de44f6e7
Masayuki Nii <nii@msyk.net>
2026-07-25 10:39:34 +0900

    Lock files updated, again.

--- dist-docs/installtags.sh
+++ dist-docs/installtags.sh
@@ -19,8 +19,9 @@ for ver in "${versionArray[@]}"; do
     git branch -D "Ver.${baseVersion}-PHP${ver}"
     git branch "Ver.${baseVersion}-PHP${ver}"
     git checkout "Ver.${baseVersion}-PHP${ver}"
+#    git pull origin "Ver.${baseVersion}-PHP${ver}"
+    git fetch origin
     git add composer.json composer.lock
-    git pull origin "Ver.${baseVersion}-PHP${ver}"
     git commit -m "Update composer.json and composer.lock for PHP ${ver}"
     git push origin "Ver.${baseVersion}-PHP${ver}"
     git tag -d "${baseVersion}-${ver}"
