8edec39deeab6f0ae9cb805afa7ae9356f6d956d
Masayuki Nii <nii@msyk.net>
2026-09-26 18:26:55 +0900

    Standardize test descriptions to specify Uppy and jQuery-File-Upload usage across databases

--- spec/run/test/specs/media2_mysql.e2e.js
+++ spec/run/test/specs/media2_mysql.e2e.js
@@ -1,7 +1,7 @@
 const AuthPageUser = require('../pageobjects/AuthPage/auth_page_credential_email_mysql.page');
 const fileTest = require('./ui_tests/file_updown')
 
-describe('File Upload and Download with MySQL', () => {
+describe('File Upload and Download with jQuery-File-Upload and MySQL', () => {
   fileTest(AuthPageUser, true)
 })
 
--- spec/run/test/specs/media3_uppy_postgresql.e2e.js
+++ spec/run/test/specs/media3_uppy_postgresql.e2e.js
@@ -1,9 +1,7 @@
 const FormPage
   = require('../pageobjects/AuthPage/auth_page_credential_basic_uppy_postgresql.page');
-
 const fileTest = require('./ui_tests/file_updown')
-const FormPage = require("../pageobjects/AuthPage/auth_page_credential_basic_uppy_mysql.page");
-describe('File Upload and Download with MySQL', () => {
+describe('File Upload and Download with Uppy and PostgreSQL', () => {
   fileTest(FormPage, false, true)
 })
 
